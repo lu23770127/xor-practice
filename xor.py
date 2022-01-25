c685447272402bfeba2e92aa3f48e38ff5ee1401
Tilda Lu <tilda.lu@trunk-studio.com>
2022-01-25 14:54:37 +0800

    feat: 💡 remove comments

--- xor.py
+++ xor.py
@@ -11,11 +11,3 @@ print("hex 2 for : %(c1)s %(c2)s" % {"c1":convert1, "c2":convert2})
 result2=hex(int(convert1, 16) ^ int(convert2, 16))
 
 print("result2 :",result2[2:])
-
-
-
-# print('%(byte1)s%(byte2)s%(byte3)s%(byte4)s' %{'byte1': list(convert1)[2], 'byte2': list(convert1)[3],'byte3': list(convert1)[4],'byte4': list(convert1)[5],})
-
-
-# hello, 0x17
-#ser.write(b'\x8a\x02\x0e\x11\x97')
